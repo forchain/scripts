60e3432f532fb23aa7e01a5643db46155b6d47ae
forchain <tony.zhouhj@gmail.com>
2018-07-24 15:39:01 +0800

    go clone

---
+++ go_clone.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+url=${1#https://}
+
+if [ -z $1 ]; then
+    echo please specify repo address
+    echo example: ./go_clone.sh https://github.com/forchain/some_repo
+    exit
+fi
+
+path=~/${url}
+
+if [ -d ${path} ]; then
+    git -C ${path} pull
+else
+    git clone https://${url} ${path}
+fi
+
+ln -s ${path} ${GOPATH}/src/${url}
